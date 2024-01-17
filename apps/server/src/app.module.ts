430743260e082433857badcd4c030b3479e4b99c
Heavyrisem <heavyrisem@gmail.com>
2024-01-17 22:44:38 +0900

    백엔드 프로젝트 셋업

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,12 +1,17 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigurationModule } from './libs/config/config.module';
+import { HttpLoggerMiddleware } from './libs/logging/http-logger.middleware';
 
 @Module({
   imports: [ConfigurationModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(HttpLoggerMiddleware).forRoutes('*');
+  }
+}
